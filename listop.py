63efa7cbaea116e91e1b550eca554da1e9ccbdf0
Ritika639 <ritika2021csds035@abesit.edu.in>
2024-07-18 15:53:46 +0530

    Add files via upload
    This file contain python program for displaying greatest between two number using list

---
+++ listop.py
@@ -0,0 +1,11 @@
+#python program to display greatest among two number
+a=int(input("enter first number"))
+b=int(input("enter second number"))
+if(a>b):
+    print("a is greatest")
+
+elif(b>a):
+    print("b is greatest")
+else:
+    print("both are equal")
+    
