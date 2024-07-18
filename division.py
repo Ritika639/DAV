ca8a5875bdbf4f234355bf8b520360a7c8bab0e3
Ritika639 <ritika2021csds035@abesit.edu.in>
2024-07-18 16:19:28 +0530

    Add files via upload
    This file contain python program to findout the number divisble 3 and even

---
+++ division.py
@@ -0,0 +1,10 @@
+#python program to print number divisible by 3
+print("taking number between 100 and 1000")
+count=00
+for i in range(100,1000):
+    if(i%3==0 and i%2==0):
+        count=count+1
+print("the number which can be divisible by 3 and are even",count)
+        
+
+       
