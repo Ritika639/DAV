f42ae83a62f4c7a3d1da520190a0061236041b24
Ritika639 <ritika2021csds035@abesit.edu.in>
2024-07-18 16:47:25 +0530

    Add files via upload
    in this we have created a dictionary and implemented some operatiion on dictionary

---
+++ dict.py
@@ -0,0 +1,32 @@
+#creating a dictionary and performing some operation
+stud_data={"stdid":'std1',"stdname":"ritika"}
+print(stud_data)
+listvar=stud_data.keys()
+print(listvar)
+stud_data.pop("stdname")
+print(stud_data)
+elementvalues=stud_data.values()
+print(elementvalues)
+listvari=stud_data.items()
+print(listvari)
+for data in stud_data:
+    print(stud_dat)
+#first dict obj
+dict_obj1={name:"amit","age":16,"standrdt":10}
+print(dict_obj1)
+dict_obj2={name:"atul","age":17,"standrdt":12}
+print(dict_obj2)
+#insertingg all the elements of dict1 in to dict2
+dict_obj1.update(dict_obj2)
+print(dict_obj1)
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
